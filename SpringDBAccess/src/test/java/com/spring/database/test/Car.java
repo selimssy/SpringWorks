69b36c5de62e42fe89132b2d740ffffb050e4fc4
selimssy <selimssy@naver.com>
2022-10-03 18:30:18 +0900

    lamda

---
+++ SpringDBAccess/src/test/java/com/spring/database/test/Car.java
@@ -0,0 +1,7 @@
+package com.spring.database.test;
+
+public interface Car {
+	
+	void run();
+	
+}
---
+++ SpringDBAccess/src/test/java/com/spring/database/test/Sonata.java
@@ -0,0 +1,10 @@
+package com.spring.database.test;
+
+public class Sonata implements Car{
+
+	@Override
+	public void run() {
+		System.out.println("소나타는 씽씽 달립니다.");
+	}
+
+}
